82a06b464b736eac55b96a36d95e37945ef55084
LORDyyyyy <mlord6271@gmail.com>
2024-01-22 16:27:38 +0200

    test

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+""" Log parsing """
+import re
+
+
+def check_input(line):
+    """ doc """
+    regex = r'^[.\d]+ - \[\d{4}-\d\d-\d\d \d\d:\d\d:\d\d\.\d{6}\] "GET \/projects\/260 HTTP\/1\.1" (\d\d\d) (\d+)$'
+    match = re.match(regex, line)
+
+    if match:
+        status_code = match.group(1)
+        file_size = int(match.group(2))
+
+        return [status_code, file_size]
+    return []
+
+
+def print_data(file_size, data={}):
+    """ doc """
+    print(f'File size: {file_size}')
+    for key, val in data.items():
+        if val == 0:
+            continue
+        print(f'{key}: {val}')
+
+
+def show_status():
+    """ doc """
+    status_code = {
+        '200': 0, '301': 0,
+        '400': 0, '401': 0,
+        '403': 0, '404': 0,
+        '405': 0, '500': 0,
+    }
+    file_size = 0
+    counter = 0
+    try:
+        while True:
+            line = input()
+            data = check_input(line)
+
+            if len(data) == 0 or data[0] not in status_code.keys():
+                continue
+            status_code[data[0]] += 1
+            file_size += data[1]
+            counter += 1
+            if counter == 10:
+                print_data(file_size, status_code)
+                counter = 0
+
+    except (KeyboardInterrupt):
+        print_data(file_size, status_code)
+        counter = 0
+
+
+if __name__ == "__main__":
+    """ main function """
+    showStatus()
