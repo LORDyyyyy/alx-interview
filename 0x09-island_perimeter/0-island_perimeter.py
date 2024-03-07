9f7e4f410fe27435776a5ad14df0bd7e59714670
LORDyyyyy <mlord6271@gmail.com>
2024-03-07 23:01:06 +0200

    solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Island Perimeter"""
+from typing import List
+
+
+def island_perimeter(grid: List[List]) -> int:
+    """
+    Returns the perimeter of the island described in grid
+
+    :param grid: the island described in grid
+    :return: the perimeter
+    """
+
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
